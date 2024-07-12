cce39d0d33e16935053e9394da2b08b4622daf5b
DmitriiOst <nugashop@yandex.ru>
2024-07-12 18:43:04 +0300

    Add: api - category

--- project/apps/category/jest.config.ts
+++ project/apps/category/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'file',
+  displayName: 'category',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/file',
+  coverageDirectory: '../../coverage/apps/category',
 };
