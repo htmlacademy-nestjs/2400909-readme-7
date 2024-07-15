f0c48eb69da88fc722e372c26250ecf5198a393c
DmitriiOst <nugashop@yandex.ru>
2024-07-15 19:25:34 +0300

    Добавит интерфейс StorableEntity

--- project/libs/shared/shared-core/src/index.ts
+++ project/libs/shared/shared-core/src/index.ts
@@ -1,5 +1,6 @@
 export { Entity } from "./lib/base/entity";
-
 export { UserRole } from "./lib/types/user-role.enum";
 export { User } from "./lib/types/user.interface";
 export { AuthUser } from "./lib/types/auth-user.interface";
+export { EntityFactory } from "./lib/interfaces/entity-factory.interface";
+export { StorableEntity } from "./lib/interfaces/storable-entity.interface";
