9c50740b66e302bb0e6697d8cbcb734f1fe3b624
DmitriiOst <nugashop@yandex.ru>
2024-07-17 15:51:25 +0300

    обавит метод comparePassword

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,5 +1,6 @@
+import { genSalt, hash, compare } from 'bcrypt';
+
 import { StorableEntity, UserRole, AuthUser, Entity } from '@project/shared-core';
-import { genSalt, hash } from 'bcrypt';
 import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
@@ -48,5 +49,9 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
     const salt = await genSalt(SALT_ROUNDS);
     this.passwordHash = await hash(password, salt);
     return this;
+  }
+
+  public async comparePassword(password: string): Promise<boolean> {
+    return compare(password, this.passwordHash);
   }
 }
