b69ad91afd39a1cd91afce12ff98cdc12073e252
DmitriiOst <nugashop@yandex.ru>
2024-07-17 15:43:37 +0300

    Добавит DTO: CreateUserDto и LoginUserDto

---
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -0,0 +1,8 @@
+export class CreateUserDto {
+  public email: string;
+  public dateBirth: string;
+  public firstname: string;
+  public lastname: string;
+  public avatarUrl: string;
+  public password: string;
+}
