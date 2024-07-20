8fe55e752c7a55dfd0208f9928b360d09439c5a4
DmitriiOst <nugashop@yandex.ru>
2024-07-20 12:20:08 +0300

    Установит пакеты mongoose и @nestjs/mongoose

--- project/libs/account/config/src/configurations/mongodb/mongo.env.ts
+++ project/libs/account/config/src/configurations/mongodb/mongo.env.ts
@@ -1,13 +1,13 @@
-import { IsNumber, IsOptional, IsString, Max, Min } from 'class-validator';
+import { IsNumber, IsOptional, IsString, Max, Min, validateOrReject } from 'class-validator';
 import { EnvValidationMessage } from './mongo.messages';
 import { DEFAULT_MONGO_PORT, MAX_PORT, MIN_PORT } from './mongo.const';
 
 export class MongoConfiguration {
   @IsString({ message: EnvValidationMessage.DBNameRequired })
-  public name: string;
+  public name!: string;
 
   @IsString({ message: EnvValidationMessage.DBHostRequired })
-  public host: string;
+  public host!: string;
 
   @IsNumber({}, { message: EnvValidationMessage.DBPortRequired })
   @Min(MIN_PORT)
@@ -16,13 +16,13 @@ export class MongoConfiguration {
   public port: number = DEFAULT_MONGO_PORT;
 
   @IsString({ message: EnvValidationMessage.DBUserRequired })
-  public user: string;
+  public user!: string;
 
   @IsString({ message: EnvValidationMessage.DBPasswordRequired })
-  public password: string;
+  public password!: string;
 
   @IsString({ message: EnvValidationMessage.DBBaseAuthRequired })
-  public authBase: string;
+  public authBase!: string;
 
   public async validate(): Promise<void> {
     await validateOrReject(this);
